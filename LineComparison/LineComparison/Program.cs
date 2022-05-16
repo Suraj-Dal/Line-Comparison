a013758d8fbd0454a22f314514e0d5c9282ab22b
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-17 00:10:49 +0530

    [Suraj]: Usecase 3 to compare two Lines is added.

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,7 +1,18 @@
 ﻿LineComparison.Shape line = new LineComparison.Shape();
 double length1 = line.Line(5, 8, 13, 7);
-double length2 = line.Line(7, 5, 6, 2);
+double length2 = line.Line(2, 5, 6, 9);
 Console.WriteLine("Length of Line 1 is: " + length1);
 Console.WriteLine("Length of Line 2 is: " + length2);
 
-Console.WriteLine(length1.Equals(length2));
+if (length1.Equals(length2) == true)
+    Console.WriteLine("Length of both lines are same.");
+else
+    Console.WriteLine("Length of Lines are not same.");
+
+Console.WriteLine(length1.CompareTo(length2));
+if (length1.CompareTo(length2) > 0)
+    Console.WriteLine("Line 1 is greater than line 2.");
+else if (length1.CompareTo(length2) < 0)
+    Console.WriteLine("Line 1 is smaller than line 2.");
+else
+    Console.WriteLine("Lines are equal.");
