9f2bcdbd777d9165054edcff10ff1f80cf20adcc
Suraj-Dal <dsurajdal10@gmail.com>
2022-05-16 20:40:32 +0530

    [Suraj]: Usecase 2 for checking equality of two Lines is added.

--- LineComparison/LineComparison/Program.cs
+++ LineComparison/LineComparison/Program.cs
@@ -1,3 +1,7 @@
 ﻿LineComparison.Shape line = new LineComparison.Shape();
 double length1 = line.Line(5, 8, 13, 7);
-Console.WriteLine("Length of Line is: " + length1);
+double length2 = line.Line(7, 5, 6, 2);
+Console.WriteLine("Length of Line 1 is: " + length1);
+Console.WriteLine("Length of Line 2 is: " + length2);
+
+Console.WriteLine(length1.Equals(length2));
